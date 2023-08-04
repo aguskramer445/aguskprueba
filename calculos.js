7b0173788614569f5ce25f1b309b2adfd6f7a704
aguskramer445 <136341916+aguskramer445@users.noreply.github.com>
2023-08-04 08:57:50 -0300

    Add files via upload

---
+++ calculos.js
@@ -0,0 +1,10 @@
+console.log('La suma es igual a', 50+50)
+console.log('La resta es igual a', 50-50)
+console.log('La multipicacion es igual a', 50*30)
+console.log('La division es igual a', 20/20)
+let  suma = 100
+let resta = 0
+let multipicacion = 1500
+let division = 1
+console.log("Variable suma", "Variable resta", "Variable multipicacion", "Variable division")
+alert("Variable suma", "Variable resta", "Variable multipicacion", "Variable division")
